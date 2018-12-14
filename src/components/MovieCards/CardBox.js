98b2db605676debaf38c001a064bfd07a0c8f7a1
Jake Burton <jburto94@yahoo.com>
2018-12-14 16:29:27 -0500

    Added smooth scrolling and navbar style adjustments

--- src/components/MovieCards/CardBox.js
+++ src/components/MovieCards/CardBox.js
@@ -1,39 +1,31 @@
 import React from 'react';
 
-const CardBox = props => {
+const CardBox = (props) => {
+	const {bgSrc, movie} = props;
 	let background = '';
 	let bgY = '';
-	if (props.bgSrc !== 'https://image.tmdb.org/t/p/w300/null' && props.bgSrc !== 'https://image.tmdb.org/t/p/w300/undefined') {
-		background = props.bgSrc;
+	if (bgSrc !== 'https://image.tmdb.org/t/p/w300/null' && bgSrc !== 'https://image.tmdb.org/t/p/w300/undefined') {
+		background = bgSrc;
 		bgY = '10%'
 	} else {
 		background = './images/unavailable.jpg';
 		bgY = '50%';
 	}
-  return (
-    <div 
-			className='card-box'
-			style={
-					{ background: `url(${background}) 50% ${bgY}`}
-			}
-    >
-			<div 
-				className='card-content'
-			>
+	return (
+		<div className='card-box'
+			style={{ background: `url(${background}) 50% ${bgY}`}}
+		>
+			<div className='card-content'>
 				<h2 className='white'>
-					{
-						props.movie.title ?
-						props.movie.title :
-						props.movie.name
-					}
+					{ movie.title ? movie.title : movie.name }
 				</h2>
 				<span className='white f4'>
 					<i className='far fa-star mr1'></i>
-					{props.movie.vote_average}
+					{movie.vote_average}
 				</span>
 			</div>
-    </div>
-  );
+		</div>
+	);
 }
 
 export default CardBox;
